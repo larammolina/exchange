b8f6869e5fe421fdbd3fb43b2f12f44d3109975d
Macarena Lara Molina <molina.macarenalara@gmail.com>
2022-08-25 18:14:05 -0300

    se sube la validacion del amount

--- src/services/getRateByBase.js
+++ src/services/getRateByBase.js
@@ -1,10 +1,10 @@
-import { BASE_URL } from "../constantes/api" 
+import { api } from "../constantes/api" 
 import axios from 'axios';
 import { response } from "express";
 
 export const getRateByBase = (symbol) => {
 
-    const response = axios.get(BASE_URL + `rates?base=${symbol}`)
+    const response = axios.get(api + `rates?base=${symbol}`)
     .then((response) => response.json());
 
     return response;
